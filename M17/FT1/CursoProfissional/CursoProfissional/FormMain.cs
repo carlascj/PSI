0e2a08d8b615991e5a748bf32de84452440e9299
Carla Jesus <95777412+carlascj@users.noreply.github.com>
2021-12-13 02:30:32 +0000

    Add files via upload

---
+++ M17/FT1/CursoProfissional/CursoProfissional/FormMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoProfissional
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void miDisciplina_Click(object sender, EventArgs e)
+        {
+            FormDisciplina formDisciplina = new FormDisciplina();
+            formDisciplina.ShowDialog();
+            Show();
+        }
+
+        private void miSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ M17/FT1/CursoProfissional/CursoProfissional/Model/ModelUtilizador.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CursoProfissional.Entities;
+using CursoProfissional.Controller;
+
+namespace CursoProfissional.Model
+{
+    class ModelUtilizador
+    {
+        ControllerUtilizador controllerUtilizador = new ControllerUtilizador();
+
+        public Utilizador EntrarMU(Utilizador utilizador)
+        {
+            try
+            {
+                return controllerUtilizador.EntrarCU(utilizador);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
